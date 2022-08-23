16122cfac55683665fdcc9fcf9bc487b0d257038
Dighitalsoluciones <hernanglomba@gmail.com>
2022-08-23 12:58:58 -0300

    iniciando backend

---
+++ src/main/java/com/beportfolio/hg/HgApplication.java
@@ -0,0 +1,13 @@
+package com.beportfolio.hg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HgApplication.class, args);
+	}
+
+}
