e65e5bbcdfdabceda007128da90028052f757f9f
Hernan Glomba <hernanglomba@gmail.com>
2022-09-01 07:38:12 -0300

    JwtTokenFilter modificado

--- src/main/java/com/beportfolio/hg/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/beportfolio/hg/security/jwt/JwtTokenFilter.java
@@ -15,7 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class JwtTokenFilter extends OncePerRequestFilter {
-    private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+    private final static Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
 
     @Autowired
     JwtProvider jwtProvider;
